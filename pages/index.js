7b5b7eda632a27d37e5512722c8575bac10c3a74
Ghabriel Rueda <68471955+ghabrielrueda@users.noreply.github.com>
2025-08-27 14:32:04 +0000

    atualiza pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Meu amor, obrigado por ser meu apoio e motivação para sempre querer crescer e me desenvolver</h1>
+    return <h1>Meu amor, obrigado por ser meu apoio e motivação para sempre querer crescer e me desenvolver pela nossa Familía. Amo muito você e nosso filhote João Emílio</h1>
 }
 
 export default Home;
