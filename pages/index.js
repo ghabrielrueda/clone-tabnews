1f7be14d02a6c3cc2e216bcfa81c037d756be654
Ghabriel Rueda <68471955+ghabrielrueda@users.noreply.github.com>
2025-09-01 23:13:03 +0000

    tira `analytics` da home

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
-import { Analytics } from "@vercel/analytics/next";
-
 function Home() {
   return (
-    <div>
-      <h1>
-        Meu amor, obrigado por ser meu apoio e motivação para sempre querer
-        crescer e me desenvolver pela nossa Familía. Amo muito você e nosso
-        filhote João Emílio. Se você também me ama, dá um tchauzinho para a
-        camera.
-      </h1>
-      <analytics />
-    </div>
+    <h1>
+      Meu amor, obrigado por ser meu apoio e motivação para sempre querer
+      crescer e me desenvolver pela nossa Familía. Amo muito você e nosso
+      filhote João Emílio. Se você também me ama, dá um tchauzinho para a
+      camera.
+    </h1>
   );
 }
 
