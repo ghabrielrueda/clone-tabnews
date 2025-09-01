f776feec62d7a40f0e4936909d1c44eed769e58e
Ghabriel Rueda <68471955+ghabrielrueda@users.noreply.github.com>
2025-09-01 23:09:45 +0000

    adiciona `<analytics>` na home

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import { Analytics } from "@vercel/analytics/next";
 
 function Home() {
   return (
-    <h1>
-      Meu amor, obrigado por ser meu apoio e motivação para sempre querer
-      crescer e me desenvolver pela nossa Familía. Amo muito você e nosso
-      filhote João Emílio. Se você também me ama, dá um tchauzinho para a
-      camera.
-    </h1>
+    <div>
+      <h1>
+        Meu amor, obrigado por ser meu apoio e motivação para sempre querer
+        crescer e me desenvolver pela nossa Familía. Amo muito você e nosso
+        filhote João Emílio. Se você também me ama, dá um tchauzinho para a
+        camera.
+      </h1>
+      <analytics />
+    </div>
   );
 }
 
